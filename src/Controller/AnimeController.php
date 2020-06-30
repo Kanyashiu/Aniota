1091fab322fb4e80200b8623f79311d469fef3a9
Kanyashiu <maazaoui.karim1@gmail.com>
2020-06-30 16:01:11 +0200

    Added details method & Update browse method in animeController

--- src/Controller/AnimeController.php
+++ src/Controller/AnimeController.php
@@ -9,11 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnimeController extends AbstractController
 {
     /**
-     * @Route("/anime", name="anime")
+     * @Route("/anime", name="anime", methods={"GET"})
      */
     public function browse()
     {
-
+        $client = HttpClient::create();
+        $animeGenre = json_decode(file_get_contents("assets/json/anime-genre.json"), true);
+        $animeRating = json_decode(file_get_contents("assets/json/anime-rating.json"), true);
+        
         if (isset($_GET['page']) && !empty($_GET['page'])) {
             $page = $_GET['page'];
         }
@@ -23,22 +26,59 @@ class AnimeController extends AbstractController
         
         if (isset( $_GET['rated']) && !empty( $_GET['rated'])) {
             $rating = $_GET['rated'];
+            
+            //! A mettre dans un service
+            // Vérification pour supprimer des notation ( controle plus fin )
+            $count = 0;
+            foreach ( $animeRating as $data ) {
+                $result = array_search($rating, $data);
+                
+                $count++;
+
+                if ( $result != false ) {
+                    // Le break permet d'éviter que le if plus bas se déclenche
+                    break;
+                }
+
+                if ( count($animeRating) == $count )
+                {
+                    throw new \Exception('Erreur 404 Rating', 404);
+                }
+            }
+            //!============================
+
+            $response = $client->request('GET', 'https://api.jikan.moe/v3/search/anime?rated=' . $rating . '&page=' . $page . '');
+        }
+        else if (isset( $_GET['genre']) && !empty( $_GET['genre'])) {
+            
+            $genre = $_GET['genre'];
+
+            //! A mettre dans un service
+            // Vérification pour supprimer des notation ( controle plus fin )
+            $count = 0;
+            foreach ( $animeGenre as $data ) {
+                $result = array_search($genre, $data);
+                
+                $count++;
+
+                if ( $result != false ) {
+                    // Le break permet d'éviter que le if plus bas se déclenche
+                    break;
+                }
+
+                if ( count($animeGenre) == $count )
+                {
+                    throw new \Exception('Erreur 404 Rating', 404);
+                }
+            }
+            //!============================
+            
+            $response = $client->request('GET', 'https://api.jikan.moe/v3/search/anime?genre=' . $genre . '&page=' . $page . '');
         }
         else {
-            $rating = 'g';
+            $response = $client->request('GET', 'https://api.jikan.moe/v3/search/anime?order_by=title');
         }
 
-        if (isset( $_GET['genre']) && !empty( $_GET['genre'])) {
-            $genre = $_GET['genre'];
-        }
-        else {
-            $genre = '0';
-        }
-        
-        
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'https://api.jikan.moe/v3/search/anime?order_by=title&rated=' . $rating . '&page=' . $page . '&genre=' . $genre . '');
-        
         // //! Pour gérer l'erreur 429 plus tard
         // //! Erreur à gerer les refresh intempestif
         // $statusCode = $response->getStatusCode();
@@ -48,7 +88,28 @@ class AnimeController extends AbstractController
         return $this->render('anime/index.html.twig', [
             'animes' => $animes['results'],
             'page' => $page,
-            'rated' => $rating,
+            'rated' => $rating ?? null,
+            'genre' => $genre ?? null,
+            'animeGenre' => $animeGenre,
+            'animeRating' => $animeRating,
         ]);
     }
+
+    /**
+     * @Route("/anime/{id}", name="anime_details", requirements={"id": "\d+"}, methods={"GET"})
+     */
+    public function details($id)
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://api.jikan.moe/v3/anime/' . $id . '');
+
+        $anime = $response->toArray();
+
+        //! Condition pour rejeter certain manga if + throw ?
+
+        return $this->render('anime/details.html.twig', [
+            'anime' => $anime
+        ]);
+    }
+
 }
