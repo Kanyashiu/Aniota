e62e4f1cb171eabb30fab1ba7d8192d19bb6ac11
Kanyashiu <maazaoui.karim1@gmail.com>
2020-07-03 15:08:43 +0200

    Adding ExcludeManga Entities

---
+++ src/Repository/ExcludeMangaRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ExcludeManga;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method ExcludeManga|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ExcludeManga|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ExcludeManga[]    findAll()
+ * @method ExcludeManga[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ExcludeMangaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ExcludeManga::class);
+    }
+
+    public function findByMalId($id)
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.mal_id = :id')
+            ->setParameter('id', $id)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
