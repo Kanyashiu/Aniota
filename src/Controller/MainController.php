ae4f718506b838cc4e4cc0e19f4d413e97652ef9
Kanyashiu <maazaoui.karim1@gmail.com>
2020-07-07 13:37:31 +0200

    Update MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Form\SearchType;
+use App\Services\YouShallNotPass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +10,13 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
+    private $youShallNotPass;
+
+    public function __construct(YouShallNotPass $youShallNotPass)
+    {
+        $this->youShallNotPass = $youShallNotPass;
+    }
+
     /**
      * @Route("/", name="home")
      */
@@ -39,6 +46,8 @@ class MainController extends AbstractController
         $content = $response->toArray();
 
         $mangas = $content['results'];
+
+        $mangas = $this->youShallNotPass->contentControlBrowseManga($mangas);
         
         sleep(2);
         $response = $httpClient->request('GET', 'https://api.jikan.moe/v3/search/anime?q='. $data .'&page=1');
@@ -46,6 +55,8 @@ class MainController extends AbstractController
         $content = $response->toArray();
 
         $animes = $content['results'];
+
+        $animes = $this->youShallNotPass->contentControlBrowseAnime($animes);
 
         foreach ($mangas as $index => $manga) {
             $resultTitle = strpos($manga['title'], $data);
