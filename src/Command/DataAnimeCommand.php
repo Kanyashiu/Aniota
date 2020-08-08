22cec85c9c2e3b09a58d94c32ca6f420b4ec505b
Kanyashiu <maazaoui.karim1@gmail.com>
2020-08-08 20:25:25 +0200

    Begin factoring DataAnimeCommand

--- src/Command/DataAnimeCommand.php
+++ src/Command/DataAnimeCommand.php
@@ -6,9 +6,12 @@ namespace App\Command;
 ini_set('memory_limit','256M');
 
 use App\Entity\ExcludeAnime;
+use App\Entity\ExcludeManga;
 use App\Repository\ExcludeAnimeRepository;
+use App\Repository\ExcludeMangaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -16,24 +19,27 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DataAnimeCommand extends Command
 {
-    protected static $defaultName = 'data:anime';
+    protected static $defaultName = 'data:command';
 
     private $client;
     private $em;
+    private $excludeMangaRepository;
     private $excludeAnimeRepository;
 
-    public function __construct( HttpClientInterface $httpClient, EntityManagerInterface $em, ExcludeAnimeRepository $excludeAnimeRepository)
+    public function __construct( HttpClientInterface $httpClient, EntityManagerInterface $em, ExcludeMangaRepository $excludeMangaRepository,ExcludeAnimeRepository $excludeAnimeRepository)
     {
         parent::__construct();
         $this->client = $httpClient;
         $this->em = $em;
+        $this->excludeAnimeRepository = $excludeAnimeRepository;
         $this->excludeAnimeRepository = $excludeAnimeRepository;
     }
 
     protected function configure()
     {
         $this
-            ->setDescription('Command scraping anime data from jiken.moe API we want to exclude')
+            ->setDescription('Command scraping manga/anime data from jiken.moe API we want to exclude')
+            ->addArgument('dataType', InputArgument::REQUIRED, 'type of data we want to scrap ( manga or anime)')
         ;
     }
 
@@ -41,8 +47,24 @@ class DataAnimeCommand extends Command
     { 
         $io = new SymfonyStyle($input, $output);
 
+        // comments
+        $dataType = strtolower($input->getArgument('dataType'));
+
+        // comments
+        if ($dataType != 'anime' && $dataType != 'manga' ) {
+            $io->error('Your argument need to be "manga" or "anime"');
+
+            return 1;
+        }
+
+        //! ======= A FAIRE
+        $excludeObject = 'App\Entity\Exclude' . ucfirst($dataType);
+        
+        $exclude = 'exclude' . ucfirst($dataType) . 'Repository';
+        //! =======
+
         // Path of the file containing the ids of the manga to exclude (based on the ids of the jikan api)
-        $file = __DIR__ .'/../../public/assets/json/anime-genre_exclude.json';
+        $file = __DIR__ .'/../../public/assets/json/' . $dataType . '-genre_exclude.json';
 
         // I retrieve the contents of the file
         $current = file_get_contents($file);
@@ -50,61 +72,63 @@ class DataAnimeCommand extends Command
         // I decode the file in order to have an array
         $currentDecode = json_decode($current, true);
 
-        // This variable will allow me to store all the manga I want to store in database
-        $animes = [];
+        // This variable will allow me to store all the manga/anime I want to store in database
+        $datas = [];
         
         // This variable allows me to get feedback in the command terminal
-        $nbAnimeTotal = 0;
+        $nbTotal = 0;
         foreach ($currentDecode as $genre) {
 
             // Actual page i want to scrap
             $page = 1;
-            // The number of total anime scrapped ( used for feedback )
-            $nbAnime = 0;
-            // The id of the anime
+            // The number of total manga/anime scrapped ( used for feedback )
+            $nb = 0;
+            // The id of the manga/anime
             $genre = $genre['id'];
 
             while(true) {
                 
                 // Request to the jikan api
-                $response = $this->client->request('GET', 'https://api.jikan.moe/v3/search/anime?genre=' . $genre . '&page='. $page .'');
+                $response = $this->client->request('GET', 'https://api.jikan.moe/v3/search/' . $dataType . '?genre=' . $genre . '&page='. $page .'');
                 // I slow down the code execution by 2 seconds to avoid error 429 from the api
                 sleep(3);
+                
                 // I'm retrieving the contents
                 $content = $response->getContent();
                 $content = $response->toArray();
                 
-                // Condition that allows you to break the while loop to go to the next anime genre
+                // Condition that allows you to break the while loop to go to the next manga/anime genre
                 if (empty($content['results'])) {
                     $io->success('Le genre n°'.$genre.' est complet, ' . ($page - 1) .' pages on été scrapé et stocké dans un tableau');
-                    $io->success( $nbAnime . ' anime ont été rajouté pour le genre n°'.$genre);
+                    $io->success( $nb . ' ' . $dataType . ' ont été rajouté pour le genre n°'.$genre);
 
                     break;
                 }
                 
-                // Loop for creating ExcludeAnime objects
+                // Loop for creating ExcludeManga/ExcludeAnime objects
                 foreach ($content['results'] as $data)
                 {
-                    // Allows the addition of anime that do not exist in bdd
+                    
+                    // Allows the addition of manga/anime that do not exist in bdd
                     if ($this->excludeAnimeRepository->findByMalId($data['mal_id']) != null)
                     {
                         continue;
                     }
 
-                    // Creation of ExcludeAnime objects and setting his properties
-                    $anime = new ExcludeAnime();
-                    $anime->setName($data['title']);
-                    $anime->setMalId($data['mal_id']);
-                    $anime->setPage($page);
-                    $anime->setGenreId($genre);
+                    // Creation of ExcludeManga/ExcludeAnime objects and setting his properties
+                    $data = new ExcludeAnime();
+                    $data->setName($data['title']);
+                    $data->setMalId($data['mal_id']);
+                    $data->setPage($page);
+                    $data->setGenreId($genre);
 
-                    // I push in the $animes array
-                    $animes[] = $anime;
+                    // I push in the $data array
+                    $datas[] = $data;
                     // I set my variable to null in order to save RAM
-                    $anime = null;
+                    $data = null;
                     // I increment my two feedback variables
-                    $nbAnime++;
-                    $nbAnimeTotal++;
+                    $nb++;
+                    $nbTotal++;
                 }
                 
                 // I set my variable to null in order to save RAM
@@ -112,7 +136,7 @@ class DataAnimeCommand extends Command
                 $content = null;
                 
                 // Used for feedback
-                echo 'La page numéro '. $page .' du genre n°' .$genre. ' à bien été lu'. PHP_EOL;
+                echo 'La page numéro '. $page .' du genre n°' .$genre. ' des ' . $dataType . ' à bien été lu'. PHP_EOL;
                 // Incrementing the variable $page to move to the next page of the api
                 $page++;
 
@@ -122,18 +146,18 @@ class DataAnimeCommand extends Command
 
         }
 
-        $nbAnime = 1;
-        foreach ($animes as $anime) {
+        $nb = 1;
+        foreach ($datas as $data) {
                 
             // Persisting my entities
-            $this->em->persist($anime);
+            $this->em->persist($data);
 
             // I set my variable to null in order to save RAM
-            $anime = null;
+            $data = null;
 
             // Used for feedback
-            echo $nbAnime . " / " . $nbAnimeTotal . " sauvegardé" . PHP_EOL;
-            $nbAnime++;
+            echo $nb . " / " . $nbTotal . " sauvegardé" . PHP_EOL;
+            $nb++;
         }
 
         // Save all my entities in database
@@ -143,7 +167,7 @@ class DataAnimeCommand extends Command
         $this->memoryUsed();
         
         // Success message when my command is over
-        $io->success( $nbAnimeTotal . ' animes exclus ont été rajouté en bdd');
+        $io->success( $nbTotal . ' ' . $dataType . 's exclus ont été rajouté en bdd');
 
         return 0;
     }
